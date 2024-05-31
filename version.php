ebf0d72b36ae25a3998845528cfebeacfde43e23
TheLD <lars@adornis.de>
2024-05-31 10:30:06 +0200

    chore: bump version

--- version.php
+++ version.php
@@ -24,7 +24,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2024052800; // The current module version (Date: YYYYMMDDXX).
+$plugin->version = 2024053100; // The current module version (Date: YYYYMMDDXX).
 $plugin->requires = 2022041900; // Requires this Moodle version.
 $plugin->component = 'mod_serlo'; // Full name of the plugin (used for diagnostics).
 //$plugin->cron      = 300;
